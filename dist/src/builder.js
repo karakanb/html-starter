eef29f8bb345877ec07df3b791fc9c22ec6c3796
Burak Karakan <burak.karakan@gmail.com>
2018-03-17 18:36:53 +0300

    Added auto browser opening for the created template.

--- dist/src/builder.js
+++ dist/src/builder.js
@@ -6,6 +6,10 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _opn = require('opn');
+
+var _opn2 = _interopRequireDefault(_opn);
+
 var _fileOperations = require('./file-operations');
 
 var _fileOperations2 = _interopRequireDefault(_fileOperations);
@@ -32,10 +36,14 @@ var _class = function () {
       _outputter2.default.success('****************************************');
       _outputter2.default.info('');
 
+      // Copy the template files.
       _outputter2.default.warning('Generating the template...');
       _fileOperations2.default.makeDir(this.directory);
       _fileOperations2.default.copy(__dirname + '/templates', this.directory);
       _outputter2.default.success('HTML skeleton has been placed to ' + this.directory + '.');
+
+      // Open the copied file in Google Chrome.
+      (0, _opn2.default)(this.directory + '/index.html', { wait: false });
 
       _outputter2.default.info('');
       _outputter2.default.success('****************************************');
--- src/builder.js
+++ src/builder.js
@@ -1,3 +1,4 @@
+import opn from 'opn';
 import File from './file-operations';
 import output from './outputter';
 
@@ -11,11 +12,14 @@ export default class {
     output.success('****************************************');
     output.info('');
 
-
+    // Copy the template files.
     output.warning('Generating the template...');
     File.makeDir(this.directory);
     File.copy(`${__dirname}/templates`, this.directory);
     output.success(`HTML skeleton has been placed to ${this.directory}.`);
+
+    // Open the copied file in Google Chrome.
+    opn(`${this.directory}/index.html`, { wait: false });
 
     output.info('');
     output.success('****************************************');
